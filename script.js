6ae655bb5ae9249a69a231b0f1eb3a62d4fe4406
Matheus Soares <matheusoares.automacao@gmail.com>
2026-08-31 22:20:33 -0300

    initial commit JS

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const checkbox = document.getElementById("toggle-style")
 
 //--Variables--//
 let fieldVerified
+let noRepeat
 
 //--Verify inputs--//
 qtd.oninput = () => {
@@ -25,6 +26,33 @@ max.oninput = () => {
     max.value = max.value.replace(/[^\d,]/g, "")
 }
 
+checkbox.addEventListener('change', (e) => {
+    if (e.target.type === 'checkbox') {
+        noRepeat = Boolean(e.target.checked)
+        console.log(noRepeat)
+    }
+})
+
+formSorter.classList.add("hidden")
+
+formArea.onsubmit = (event) => {
+    event.preventDefault()
+
+    if (!validFields()) {
+        return
+    }
+
+    const numeros = sorter(qtd.value, min.value, max.value, noRepeat)
+    console.log(numeros) 
+    
+    formArea.classList.add("hidden")
+    formSorter.classList.remove("hidden")
+}
+
+
+//--FUNCTION AREA--//
+
+// Valid Fields
 function validFields() {
     try {
         // Verify null field       
@@ -42,21 +70,50 @@ function validFields() {
 }
 
 
-formSorter.classList.add("hidden")
-
-formArea.onsubmit = (event) => {
-    event.preventDefault()
-
-    if (!validFields()) {
-        return
-    }
-    formArea.classList.add("hidden")
-    formSorter.classList.remove("hidden")
+// SoarterNumber
+function sorterNumber(min, max) {
+    min = Number(min)
+    max = Number(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-checkbox.addEventListener('change', (e)=>{
-   
-})
+function noRepeatSorter(min, max, qtd) {
+    min = Number(min)
+    max = Number(max)
+    qtd = Number(qtd)
+
+    const idleNumbers = []
+    for (let i = min; i <= max; i++) idleNumbers.push(i);
+
+    const resultNoRepeat = [];
+    for (let i = 0; i < qtd; i++) {
+        const randomIndex = Math.floor(Math.random() * idleNumbers.length)
+        resultNoRepeat.push(idleNumbers[randomIndex]);
+        idleNumbers.splice(randomIndex, 1);
+    }
+
+    return resultNoRepeat;
+}
+
+
+// Sorter
+function sorter(qtd, min, max, noRepeatNumber) {
+
+    min = Number(min)
+    max = Number(max)
+    qtd = Number(qtd)
+
+       if (noRepeatNumber) {
+        return sortearSemRepetir(min, max, qtd)
+    }
+
+    const result = [];
+    for (let i = 0; i < qtd; i++) {
+        result.push(sorterNumber(min, max));
+    }
+    return result;
+}
+
 
 
 
